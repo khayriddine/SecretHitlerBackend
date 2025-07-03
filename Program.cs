3e9f2e201a34f5c993e8f806c2314e3a68be7c21
khyari <khayribattikh@gmail.com>
2025-07-03 04:57:15 +0200

    configure cors variable

--- Program.cs
+++ Program.cs
@@ -2,11 +2,13 @@ using SecretHitlerBackend.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
+var corsOrigins = builder.Configuration["AllowedOrigins"]?.Split(';')
+                  ?? new[] { "http://localhost:4200" };
 // Add CORS (place this BEFORE AddSignalR)
 builder.Services.AddCors(options => {
     options.AddPolicy("SecretHitlerFront",
         policy => {
-            policy.WithOrigins("http://localhost:4200") // Your Angular URL
+            policy.WithOrigins(corsOrigins) // Your Angular URL
                   .AllowAnyHeader()
                   .AllowAnyMethod()
                   .AllowCredentials(); // Required for SignalR
